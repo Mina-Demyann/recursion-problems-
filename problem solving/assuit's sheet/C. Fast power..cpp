753cb8a9ac93766d763b01b784a7a08338157126
Mina-Demyann <151796399+Mina-Demyann@users.noreply.github.com>
2024-06-07 18:11:55 +0300

    Update C. Fast power..cpp

--- problem solving/assuit's sheet/C. Fast power..cpp
+++ problem solving/assuit's sheet/C. Fast power..cpp
@@ -1,4 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std ;
 #include <iostream>
+
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <bitset>
+#include <map>
+
+#include <deque>
+#include <string>
+
+#include <algorithm>
+#include <numeric>
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+#include <ctime>
+#include <cmath>
+#define rep(i,a,b) for(ll i=(ll)(a);i<(ll)(b);i++)
+
+#define per(i,a,b) for(ll i=(ll)(a);i>(ll)(b);i--)
+#define each(a, x) for (auto &a : x)
+#define pb push_back
+#define pbk pop_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define all(x) (x).begin(), (x).end()
+#define foreach(i, a) for (__typeof((a).begin()) i = (a).begin(); i != (a).end(); ++i)
+#define len(a) ((int) (a).size())
+#include <numeric>
+#include "string"
+#define rall(my) my.rbegin(), my.rend()
+#define ll long long
+#define el "\n"
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 const int MOD = 1000000007;
 
